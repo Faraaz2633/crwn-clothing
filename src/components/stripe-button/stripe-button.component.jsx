895ffc2e4858b1dc21c7f78f4eeee918c5901a80
Mohammed Mothaseem Faraz <73326287+Faraaz2633@users.noreply.github.com>
2021-08-28 13:19:13 +0530

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51JTK7HSB6fPdWK0X6ckabXTqFmcgekaL8OKdQM7ga48qLthchQwjSjHRCU0NPKwCMB4PLUv5pMvV2R7d4y3fSvjc00ISY5cwMe';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='FRZ Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
